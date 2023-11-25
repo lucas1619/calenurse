ef5ef2e1b7812afe08ae663ef3665104b3bdb898
Lucas Alejandro Moreno Olivos <lmoreno@progrez.pe>
2023-11-25 16:51:25 -0500

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import * as express from "express"
-import { Request, Response } from "express"
-import { User } from "./entity/user.entity"
 import { myDataSource } from "./app-data-source"
 
 // establish database connection
@@ -20,25 +18,6 @@ myDataSource
 // create and setup express app
 const app = express()
 app.use(express.json())
-
-// register routes
-app.get("/users", async function (req: Request, res: Response) {
-    const users = await myDataSource.getRepository(User).find()
-    res.json(users)
-})
-
-
-app.post("/users", async function (req: Request, res: Response) {
-    const user = await myDataSource.getRepository(User).create(req.body)
-    const results = await myDataSource.getRepository(User).save(user)
-    return res.send(results)
-})
-
-
-app.delete("/users/:id", async function (req: Request, res: Response) {
-    const results = await myDataSource.getRepository(User).delete(req.params.id)
-    return res.send(results)
-})
 
 // start express server
 app.listen(3000)
