05bbadae1ecd1d4dc732335130af360eda86d7db
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2023-11-25 19:08:26 -0500

    set sync to false

--- src/routes/v1/desired-schedule/index.ts
+++ src/routes/v1/desired-schedule/index.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.post("/make", (req : Request, res : Response) => {
     try {
-        const { area_id } = req.body;
+        const {area_id} = req.body;
         let date = new Date();
         const monday = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + 1);
         const sunday = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay() + 7);
